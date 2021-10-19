1a4ae1d013d50a640c47a3a9d06d8de6b52ce3e0
Denis Ackerman <zahardrus@gmail.com>
2021-10-19 17:26:15 +0300

    fix loader add bgi

--- src/components/MyLoader/MyLoader.jsx
+++ src/components/MyLoader/MyLoader.jsx
@@ -1,7 +1,7 @@
 import './MyLoader.css'
 export const MyLoader = ({text}) => {
     return(
-        <div>
+        <div className={'loader-content'}>
             <div className="lds-circle">
                 <div className={'text-center'}/>
             </div>
