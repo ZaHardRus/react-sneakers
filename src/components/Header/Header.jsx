16435b8b20cef901c95fb6b71fb879ea9c927a22
Denis Ackerman <zahardrus@gmail.com>
2021-10-19 16:43:30 +0300

    change header layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import s from './Header.module.scss'
-import HeaderLogo from "../../assets/header-logo.png";
-import HeaderCart from "../../assets/header-bag.svg";
-import HeaderLike from "../../assets/header-like.svg";
+import HeaderLogo from "../../assets/header-logo1.png";
+import HeaderCart from "../../assets/header-bag2.svg";
+import HeaderLike from "../../assets/header-like1.svg";
 import HeaderAvatar from "../../assets/header-avatar.svg";
 import {Link} from "react-router-dom";
 import {useContext} from "react";
@@ -14,28 +14,37 @@ export const Header = ({cartLength,favoritesLength,...props}) => {
         <header className={s.header}>
             <Link to={'/'}>
                 <div className={s.headerLeft}>
-                    <img width={40} height={40} src={HeaderLogo} alt="header-logo"/>
+                    <img width={70} height={70} src={HeaderLogo} alt="header-logo"/>
                     <div className={s.headerLeftInfo}>
-                        <h3 className={s.title}>REACT SNEAKERS</h3>
+                        <h2 className={s.title}>STREET SNEAKERS</h2>
                         <p className={s.text}>Магазин кроссовок</p>
                     </div>
                 </div>
             </Link>
             <ul className={s.headerRight}>
+
                 <Link to={'/favorites'}>
                     <li className={s.headerLike}>
-                        <img width={24} height={24} src={HeaderLike} alt="favorites=link"/>
-                        <p>{favoritesLength}</p>
+                        <h5>Избранное</h5>
+                        <div className={s.headerGroup}>
+                            <img width={40} height={40} src={HeaderLike} alt="favorites=link"/>
+                            <p>{favoritesLength}</p>
+                        </div>
+
                     </li>
                 </Link>
                 <li className={s.headerCart} onClick={() => props.setCartOpened(true)}>
-                    <img width={24} height={24} src={HeaderCart} alt="cart-link"/>
-                    <p>{cartLength}</p>
+                    <h5>Корзина</h5>
+                    <div className={s.headerGroup}>
+                        <img width={40} height={40} src={HeaderCart} alt="cart-link"/>
+                        <p>{cartLength}</p>
+                    </div>
+                    {cartLength ? <h5 className={s.headerPrice}><span>{formatPrice(totalCost)} руб.</span></h5> : null}
                 </li>
-                {cartLength ? <li className={s.headerPrice}><span>{formatPrice(totalCost)} руб.</span></li> : null}
                 <Link to={'/order'}>
                     <li className={s.headerAvatar}>
-                        <img width={24} height={24} src={HeaderAvatar} alt="order-link"/>
+                        <h5>Мои заказы</h5>
+                        <img width={40} height={40} src={HeaderAvatar} alt="order-link"/>
                     </li>
                 </Link>
             </ul>
--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,37 +1,28 @@
 import s from './ProductItem.module.scss'
-import {useRef, useState} from "react";
+import {useState} from "react";
+import Slider from 'react-touch-drag-slider'
 
 export const ProductItem = ({
-                                id, price, image = '',
-                                loading, title = '', description = '',sizes,
+                                id, price, image = [],
+                                loading, title = '', description = '',
                                 redirect, addToFavorites, isItemFavorited
                             }) => {
     let [slide, setSlide] = useState(0)
-    const slider = useRef()
-    console.log(slider)
+
     const next = () => {
-        slider.current.className = 'swap'
-        setTimeout(()=>{
-            if (slide >= image.length - 1) {
-                setSlide(0)
-            } else {
-                setSlide(prev => prev + 1)
-            }
-            slider.current.className = ''
-        },[1500])
+        if (slide >= image.length - 1) {
+            return null
+        } else {
+            setSlide(prev => prev + 1)
+        }
     }
     const prev = () => {
-        slider.current.className = 'swap'
-        setTimeout(()=>{
-            if (slide <= 0) {
-                setSlide(image.length - 1)
-            } else {
-                setSlide(prev => prev - 1)
-            }
-            slider.current.className = ''
-        },[1500])
+        if (slide <= 0) {
+            return null
+        } else {
+            setSlide(prev => prev - 1)
+        }
     }
-
     const clickFavorites = () => {
         addToFavorites()
     }
@@ -39,26 +30,37 @@ export const ProductItem = ({
         <div className={s.productWrapper}>
             <h1 className={s.title}>{title}</h1>
             <div className={s.product}>
-                <div className={s.arrow} onClick={prev}>
-                    <svg className={s.arrowLeft} viewBox="0 0 60 100">
-                        <path d="M 50,0 L 60,10 L 20,50 L 60,90 L 50,100 L 0,50 Z"/>
-                    </svg>
-                </div>
                 <div className={s.image}>
-                    {
-                        loading ? <p className={s.loading}>Загрузка изображения...</p> :
-                            <img className='slider' ref={slider} src={image[slide]} alt="sneaker"/>
-                    }
+                    {loading ? <p className={s.loading}>Загрузка изображения...</p> :
+                        <Slider
+                            activeIndex={slide}
+                            threshHold={200}
+                            transition={0.8}
+                            scaleOnDrag={true}
+                        >
+                            {image.map((el, index) => (
+                                <img src={el} key={index } alt={'title'}/>
+                            ))}
+                        </Slider>}
                 </div>
-                <div className={s.arrow} onClick={next}>
-                    <svg className={s.arrowRight} viewBox="0 0 60 100">
-                        <path d="M 50,0 L 60,10 L 20,50 L 60,90 L 50,100 L 0,50 Z"/>
-                    </svg>
+                <div className={s.arrowWrapper}>
+                    <div className={s.arrow} onClick={prev}>
+                        <svg className={s.arrowLeft} viewBox="0 0 60 100">
+                            <path d="M 50,0 L 60,10 L 20,50 L 60,90 L 50,100 L 0,50 Z"/>
+                        </svg>
+                    </div>
+                    <div className={s.arrow} onClick={next}>
+                        <svg className={s.arrowRight} viewBox="0 0 60 100">
+                            <path d="M 50,0 L 60,10 L 20,50 L 60,90 L 50,100 L 0,50 Z"/>
+                        </svg>
+                    </div>
                 </div>
+
             </div>
             <p className={s.description}>{description}</p>
             <div className="btns-wrapper">
-                <button className='red_button' onClick={clickFavorites}>{isItemFavorited(id)?'Убрать из избранного':'Добавить в избранное'}</button>
+                <button className='red_button'
+                        onClick={clickFavorites}>{isItemFavorited(id) ? 'Убрать из избранного' : 'Добавить в избранное'}</button>
                 <button className='green_button' onClick={redirect}>На главную</button>
             </div>
         </div>
